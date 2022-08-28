4c67e3c795bfd0b8ea23d3edd1328d6d0823b993
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-08-28 17:03:32 +0300

    Решена первая задача

--- HM_1/Program.cs
+++ HM_1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);        // Вывод приглашения
+    string strValue;                      // Объявление переменной для ввода строки
+    strValue = Console.ReadLine() ?? "0"; // Вводим строку с консоли (с консоли можно ввести только строку)
+    int value = int.Parse(strValue);      // Преобразование строки в целое число
+    return value;
+}
+
+int first = Prompt("Введите первое число: ");
+int second = Prompt("Введите второе число: ");
+int max;
+if (first != second)
+{
+    if (first > second)
+    {
+        max = first;
+        System.Console.WriteLine($"Наибольшее значение из {first} и {second} равно: {max}");
+    }
+    else
+    {
+        max = second;
+        System.Console.WriteLine($"Наибольшее значение из {first} и {second} равно: {max}");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введенные числа равны");
+}
