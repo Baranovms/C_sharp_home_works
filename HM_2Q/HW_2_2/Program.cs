0c1c3224a8951bbb51f5f6263ea0747008c80427
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-03 16:50:13 +0300

    задания 2 изменение кода

--- HM_2Q/HW_2_2/Program.cs
+++ HM_2Q/HW_2_2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 
-int number = new Random().Next(100,999);
+int number = new Random().Next(100,1000);
 int new_numb = int.Parse(number.ToString().Remove(1, 1));
 Console.WriteLine($"Введенное значение {number} после удаления второго значение {new_numb}");
