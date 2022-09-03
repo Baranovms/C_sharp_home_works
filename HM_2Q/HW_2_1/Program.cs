58d6bf3f30d075151e80ae5440fdb71333f98f9f
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-03 19:43:41 +0300

    Изменено решение задачи 1, на математическое

--- HM_2Q/HW_2_1/Program.cs
+++ HM_2Q/HW_2_1/Program.cs
@@ -1,13 +1,22 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
-System.Console.Write("Введите трехзначное число: ");
-string value = Console.ReadLine();
-if (value.Length == 3)
+int Prompt(string message)
 {
-    char secondChar = value[value.Length-2];
+    System.Console.Write(message);       
+    string strValue;                      
+    strValue = Console.ReadLine() ?? "0"; 
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int value = Prompt("Введите трехзначное число: ");
+if ((value < 1000) && (value > 99))
+{
+    int secondChar = (value / 10) % 10;
     System.Console.WriteLine($"Среднее значение числа {value} равно {secondChar}");
 }
 else
 {
-    System.Console.WriteLine("Введенное значение вне диапазона");
+    System.Console.WriteLine("Число не является трехзначным");
 }
+
+
