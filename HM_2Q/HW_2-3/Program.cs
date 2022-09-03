1fb26170c95a2a0647485404c39bf85c11a3b471
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-03 16:59:13 +0300

    задания 3

--- HM_2Q/HW_2-3/Program.cs
+++ HM_2Q/HW_2-3/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+int Prompt(string message)
+{
+    System.Console.Write(message);       
+    string strValue;                      
+    strValue = Console.ReadLine() ?? "0"; 
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int value = Prompt("Введите число: ");
+string valueText = Convert.ToString(value);
+if (valueText.Length > 2)
+{
+  System.Console.WriteLine($"Введенное число {value}, его третья цифра равна {valueText[2]}");
+}
+else {
+  Console.WriteLine($"Введенное число {value} не имеет третьей цифры");
+}
