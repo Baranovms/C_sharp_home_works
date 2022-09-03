c1d89d1d66e189ebca09fc54a5e3ffbd2b08abe5
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-03 16:47:14 +0300

    задания 1 и 2

---
+++ HM_2Q/HW_2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+System.Console.Write("Введите трехзначное число: ");
+string value = Console.ReadLine();
+if (value.Length == 3)
+{
+    char secondChar = value[value.Length-2];
+    System.Console.WriteLine($"Среднее значение числа {value} равно {secondChar}");
+}
+else
+{
+    System.Console.WriteLine("Введенное значение вне диапазона");
+}
---
+++ HM_2Q/HW_2_2/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100,999);
+int new_numb = int.Parse(number.ToString().Remove(1, 1));
+Console.WriteLine($"Введенное значение {number} после удаления второго значение {new_numb}");
