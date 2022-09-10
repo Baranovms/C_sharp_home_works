e5dbf963fc8f6848b84685ba2cc6533a741b9aa1
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-10 17:50:16 +0300

    добавлены задачи 1 и 2, из ДЗ_4

---
+++ HM_4Q/HW_4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double Promt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+//System.Console.WriteLine($"Число {number} возвели в степень {degree}: {Math.Pow(number, degree)}"); //самое простое решение
+
+
+double stepen(double number)
+{
+    double newNumber = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        newNumber = number * newNumber;
+    }
+    return newNumber;
+}
+
+double number = Promt("Введите возводимое число: ");
+double degree = Promt("Введите степень: ");
+System.Console.WriteLine($"Число {number} возвели в степень {degree}: {stepen(number)}");
---
+++ HM_4Q/HW_4_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Promt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigit(int num)
+{
+    int sumNumbers = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        sumNumbers += digit;
+        num /= 10;
+    }
+    return sumNumbers;
+}
+int num = Promt("Введите число: ");
+System.Console.WriteLine($"Сумма цифр в числе {num} равна {SumDigit(num)}");
+
