fae42d380422ffb6aaf83936d628882350426a6c
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-11 19:44:12 +0300

    добавлена задача 3 из ДЗ_5

---
+++ HM_5Q/HW_5_3/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 3: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit)
+    {
+        return number;
+    }
+    throw new Exception("Вы ввели не число");
+}
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + ", ");
+    }
+    System.Console.WriteLine();
+}
+
+
+int MaxElement(int[] array)
+{
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int MinElement(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int DiffMaxMin(int[] array)
+{
+    
+    int difference = MaxElement(array) - MinElement(array);
+    return difference;
+}
+
+const int MIN_ELEMENTS = 0;
+const int MAX_ELEMENTS = 99;
+
+int length = Prompt("Введите длину массива: ");
+int[] array = GenerateArray(length, MIN_ELEMENTS, MAX_ELEMENTS);
+PrintArray(array);
+Console.WriteLine($"Разность максимального {MaxElement(array)} и минимального {MinElement(array)} элементов массива равна: {DiffMaxMin(array)}");
