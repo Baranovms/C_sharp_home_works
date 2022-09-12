700c06b8704bf721a87d71853d78223c783e6fed
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-12 21:12:40 +0300

    добавлена задача 4 ДЗ_4

--- HM_4Q/HW_4_4/Program.cs
+++ HM_4Q/HW_4_4/Program.cs
@@ -27,59 +27,24 @@ void PrintArray(int[] array)
         System.Console.Write($"{array[i]} ");
     }
     System.Console.WriteLine();
-    int index1 = 0;
-    int temp = 0;
     int max1 = array[0];
-    int max2 = array[0];
-
+    int max2 = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] > max1)
+        if (max1 < array[i])
         {
-            max1 = array[i];
-            index1 = i;
             max2 = max1;
-            array[0] = max1;
+            max1 = array[i];
         }
-        // temp = array[index1];
-        // array[index1] = array[0];
-        // array[0] = temp;
-        System.Console.Write($"{array[i]} ");
-        if (array[i] > array[index1])
+        else if (max1 != array[i] && max2 < array[i])
         {
             max2 = array[i];
-            if (max2 != max1)
-            {
-
-            }
-
         }
     }
     System.Console.WriteLine();
-    System.Console.WriteLine($"Первый максимум: {max1}");
     System.Console.WriteLine($"второй максимум: {max2}");
-    System.Console.WriteLine($"Индекс Первый максимум: {index1}");
-
 }
 
-// int MaxArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length - 1; i++)
-//     {
-//         int maxPosition = i;
-//         for (int j = i + 1; j < array.Length; j++)
-//         {
-//             if (array[j] > array[maxPosition])
-//             {
-//                 maxPosition = j;
-//             }
-//         }
-//         int temp = array[i];
-//         array[i] = array[maxPosition];
-//         array[maxPosition] = temp;
-//     }
-//     return array[maxPosition];
-// }
 
 PrintArray(CreateArray(8));
 
