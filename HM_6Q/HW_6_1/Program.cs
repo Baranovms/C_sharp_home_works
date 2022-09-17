72c32e193a2f010b5e683c0da0a04bd30aa45182
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-17 16:39:24 +0300

    добавлена задача 1 из дз_6

---
+++ HM_6Q/HW_6_1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit)
+    {
+        return number;
+    }
+    throw new Exception("Вы ввели не число");
+}
+
+int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1} значение: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + ", ");
+    }
+    System.Console.WriteLine();
+}
+
+int CountNotNull(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = Prompt("Введите Количество вводимых значений: ");
+int[] arr = CreateArray(length);
+Console.Clear();
+PrintArray(arr);
+Console.Write($"количество значенией больше нуля: {CountNotNull(arr)}");
