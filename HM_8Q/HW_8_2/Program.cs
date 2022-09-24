8abf26227a720f490f19492cf894784fbe21634d
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-24 16:39:49 +0300

    Задача 2 ДЗ 8

--- HM_8Q/HW_8_2/Program.cs
+++ HM_8Q/HW_8_2/Program.cs
@@ -1,4 +1,12 @@
-﻿// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+﻿// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер 
+// строки с наименьшей суммой элементов: 1 строка
 
 Console.Clear();
 
@@ -30,3 +38,28 @@ void PrintArray(int[,] array)
     System.Console.WriteLine();
 }
 
+int FindNumbRowMinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    return minSumRow;
+}
+
+int[,] myArray = GenerateArray(3,4,0,10);
+PrintArray(myArray);
+Console.Write($"Номер строки с наименьшей суммой элементов: {FindNumbRowMinSumElements(myArray) + 1} строка");
