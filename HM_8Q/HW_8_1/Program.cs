a7a08deeb9f24df8755e3efea27f36ffcb66ac50
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-24 16:27:44 +0300

    Задача 1 ДЗ 8

---
+++ HM_8Q/HW_8_1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+
+int[,] GenerateArray(int rowLength, int colLength, int minRange, int maxRange)
+{
+    var array = new int[rowLength, colLength];
+    var random = new Random();
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(minRange, maxRange + 1);
+
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void SortedArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int[,] myArray = GenerateArray(3,4,0,10);
+PrintArray(myArray);
+SortedArrayLines(myArray);
+PrintArray(myArray);
