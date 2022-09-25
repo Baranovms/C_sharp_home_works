52585bde3a1e02bb26070bb76c47c17c6f07d3b1
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-09-25 18:40:28 +0300

    Задача 1-3  ДЗ 9я

---
+++ HM_9Q/HW_9_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    bool isNumber = int.TryParse(strValue, out int value);
+    if (isNumber)
+    {
+        return value;
+    }
+    throw new Exception("Данное значение не возможно преобразовать в число");
+}
+
+void SumNumbers(int startValue, int finishVAlue, int summ)
+{
+    if (startValue > finishVAlue)
+    {
+        System.Console.WriteLine($"Сумма элементов равна: {summ}");
+        return;
+    }
+    summ = summ + (startValue++);
+    SumNumbers(startValue, finishVAlue, summ);
+}
+
+int first = Prompt("Введите начальное число: ");
+int second = Prompt("Введите конечное число: ");
+SumNumbers(first, second,0);
---
+++ HM_9Q/HW_9_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    bool isNumber = int.TryParse(strValue, out int value);
+    if (isNumber)
+    {
+        return value;
+    }
+    throw new Exception("Данное значение не возможно преобразовать в число");
+}
+
+int Akkerman(int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    if (numM > 0 && numN == 0)
+    {
+        return Akkerman(numM - 1, 1);
+    }
+    if (numM > 0 && numN > 0)
+    {
+        return Akkerman(numM - 1, Akkerman(numM, numN - 1));
+    }
+    return Akkerman(numM, numN);
+}
+
+
+int numberM = Prompt("Введите параметр m:");
+int numberN = Prompt("Введите параметр n:");
+System.Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {Akkerman(numberM, numberN)}");
